2038f6000652d1eab29ad4a744fd12a25a2580c8
Dong ki <119654357+wtfwtfs@users.noreply.github.com>
2023-09-21 14:18:54 +0900

    Add files via upload

---
+++ aa10_rpt02/aa10info.js
@@ -0,0 +1,7 @@
+module.exports = function (id,name,phone) {
+    console.log("My Info");
+    console.log("ID : " + id);
+    console.log("Name : " + name);
+    console.log("Phone : " + phone +"\n");
+    
+}
---
+++ aa10_rpt02/index_aa10.js
@@ -0,0 +1,7 @@
+//index_aa10.js
+
+var myinfo = require('./aa10info');
+
+myinfo("aa10", "Redwoods", 010-1234-5678);
+
+myinfo("aa10", "Comsi", '010-5678-1234');
